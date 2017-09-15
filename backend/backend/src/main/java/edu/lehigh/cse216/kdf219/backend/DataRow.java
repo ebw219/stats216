43bb925e607c45562020a163616b8df6404ced42
kfrank15 <kfrank15@kentdenver.org>
2017-09-15 18:42:11 -0400

    added initial files

---
+++ backend/backend/src/main/java/edu/lehigh/cse216/kdf219/backend/DataRow.java
@@ -0,0 +1,66 @@
+package edu.lehigh.cse216.kdf219.backend;
+
+import java.util.Date;
+
+/**
+ * DataRow holds a row of information.  A row of information consists of
+ * an identifier, strings for a "title" and "content", and a creation date.
+ * 
+ * Because we will ultimately be converting instances of this object into JSON
+ * directly, we need to make the fields public.  That being the case, we will
+ * not bother with having getters and setters... instead, we will allow code to
+ * interact with the fields directly.
+ */
+public class DataRow {
+    /**
+     * The unique identifier associated with this element.  It's final, because
+     * we never want to change it.
+     */
+    public final int mId;
+
+    /**
+     * The title for this row of data
+     */
+    public String mTitle;
+
+    /**
+     * The content for this row of data
+     */
+    public String mContent;
+
+    /**
+     * The creation date for this row of data.  Once it is set, it cannot be 
+     * changed
+     */
+    public final Date mCreated;
+
+    /**
+     * Create a new DataRow with the provided id and title/content, and a 
+     * creation date based on the system clock at the time the constructor was
+     * called
+     * 
+     * @param id The id to associate with this row.  Assumed to be unique 
+     *           throughout the whole program.
+     * 
+     * @param title The title string for this row of data
+     * 
+     * @param content The content string for this row of data
+     */
+    DataRow(int id, String title, String content) {
+        mId = id;
+        mTitle = title;
+        mContent = content;
+        mCreated = new Date();
+    }
+
+    /**
+     * Copy constructor to create one datarow from another
+     */
+    DataRow(DataRow data) {
+        mId = data.mId;
+        // NB: Strings and Dates are immutable, so copy-by-reference is safe
+        mTitle = data.mTitle;
+        mContent = data.mContent;
+        mCreated = data.mCreated;
+    }
+}
---
+++ backend/backend/src/main/java/edu/lehigh/cse216/kdf219/backend/StructuredResponse.java
@@ -0,0 +1,41 @@
+package edu.lehigh.cse216.kdf219.backend;
+
+/**
+ * StructuredResponse provides a common format for success and failure messages,
+ * with an optional payload of type Object that can be converted into JSON.
+ * 
+ * NB: since this will be converted into JSON, all fields must be public.
+ */
+public class StructuredResponse {
+    /**
+     * The status is a string that the application can use to quickly determine
+     * if the response indicates an error.  Values will probably just be "ok" or
+     * "error", but that may evolve over time.
+     */
+    public String mStatus;
+
+    /**
+     * The message is only useful when this is an error, or when data is null.
+     */
+    public String mMessage;
+
+    /**
+     * Any JSON-friendly object can be referenced here, so that we can have a
+     * rich reply to the client
+     */
+    public Object mData;
+
+    /**
+     * Construct a StructuredResponse by providing a status, message, and data.
+     * If the status is not provided, set it to "invalid".
+     * 
+     * @param status The status of the response, typically "ok" or "error"
+     * @param message The message to go along with an error status
+     * @param object An object with additional data to send to the client
+     */
+    public StructuredResponse(String status, String message, Object data) {
+        mStatus = (status != null) ? status : "invalid";
+        mMessage = message;
+        mData = data;
+    }
+}
