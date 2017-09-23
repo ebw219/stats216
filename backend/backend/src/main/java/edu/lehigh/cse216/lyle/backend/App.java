0a93ffea9c928e3d1e013c3ecac16af26b5202db
Kelli Frank <kdf219@lehigh.edu>
2017-09-23 13:49:02 -0400

    making changes

--- backend/backend/src/main/java/edu/lehigh/cse216/lyle/backend/App.java
+++ backend/backend/src/main/java/edu/lehigh/cse216/lyle/backend/App.java
@@ -13,6 +13,9 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) {
 
+	// Get the port on which to listen for requests
+	Spark.port(getIntFromEnv("PORT", 4567));
+
 	// gson provides us with a way to turn JSON into objects, and objects
 	// into JSON.
 	//
@@ -33,6 +36,8 @@ public class App {
 	String port = env.get("POSTGRES_PORT");
 	String user = env.get("POSTGRES_USER");
 	String pass = env.get("POSTGRES_PASS");
+
+	System.out.println("\nuser: " + user + "\n");
 
 	final Database database = Database.getDatabase(ip, port, user, pass); //changed to database instead of datastore
 	if (!database.tableDoesExist()) {
@@ -148,6 +153,21 @@ public class App {
 
     }
 
-    
+/**
+ * Get an integer environment varible if it exists, and otherwise return the
+ * default value.
+ * 
+ * @envar      The name of the environment variable to get.
+ * @defaultVal The integer value to use as the default if envar isn't found
+ * 
+ * @returns The best answer we could come up with for a value for envar
+ */
+static int getIntFromEnv(String envar, int defaultVal) {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get(envar) != null) {
+        return Integer.parseInt(processBuilder.environment().get(envar));
+    }
+    return defaultVal;
+}    
 
 }
