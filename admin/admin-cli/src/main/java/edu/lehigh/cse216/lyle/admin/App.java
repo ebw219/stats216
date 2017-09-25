e8198632b04390656b546c11aeee416a8bd04412
tjj218 <tjj218@lehigh.edu>
2017-09-25 13:33:00 -0400

    added skeleton

---
+++ admin/admin-cli/src/main/java/edu/lehigh/cse216/lyle/admin/App.java
@@ -0,0 +1,13 @@
+package edu.lehigh.cse216.lyle.admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
