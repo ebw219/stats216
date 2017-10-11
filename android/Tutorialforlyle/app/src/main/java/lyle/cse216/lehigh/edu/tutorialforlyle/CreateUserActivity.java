b233383eda30f8fcf4cb3fa7264f427c7e337f59
Kelli Frank <kdf219@lehigh.edu>
2017-10-11 18:39:27 -0400

    ability to register user

--- android/Tutorialforlyle/app/src/main/java/lyle/cse216/lehigh/edu/tutorialforlyle/CreateUserActivity.java
+++ android/Tutorialforlyle/app/src/main/java/lyle/cse216/lehigh/edu/tutorialforlyle/CreateUserActivity.java
@@ -3,14 +3,28 @@ package lyle.cse216.lehigh.edu.tutorialforlyle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 /**
  * Created by Kelli on 10/8/17.
  * Phase 3
  */
 
+
 public class CreateUserActivity extends AppCompatActivity {
+String url = "https://sleepy-dusk-34987.herokuapp.com/users";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +36,51 @@ public class CreateUserActivity extends AppCompatActivity {
                 Intent input = new Intent(getApplicationContext(), LoginActivity.class);
                 input.putExtra("label_contents", "Cancel");
                 startActivityForResult(input, 789);
+            }
+        });
+
+        findViewById(R.id.submitCreate).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                String username = ((TextView) findViewById(R.id.newUsername)).getText().toString();
+                String name = ((TextView) findViewById(R.id.newName)).getText().toString();
+                String email = ((TextView) findViewById(R.id.newEmail)).getText().toString();
+
+                if(username.isEmpty() || name.isEmpty() || email.isEmpty()){
+                    findViewById(R.id.invalidField).setVisibility(View.VISIBLE);
+                } else {
+                    StringRequest newReq = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("lyle", response);
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("lyle", "That POST didn't work!");
+                            Log.e("lyle", " If your sendPutRoute is about 10 line up the log, " +
+                                    "the message parsed correctly, but the server rejected it");
+                        }
+                    })
+                    {
+                        @Override
+                        public byte[] getBody() throws AuthFailureError {
+                            HashMap<String, String> params = new HashMap<String, String>();
+                            params.put("email", ((TextView) findViewById(R.id.newEmail)).getText().toString());
+                            params.put("username", ((TextView) findViewById(R.id.newUsername)).getText().toString());
+                            params.put("realname", ((TextView) findViewById(R.id.newName)).getText().toString());
+                            Log.d("lyle", new JSONObject(params).toString());
+                            return new JSONObject(params).toString().getBytes();
+                        }
+
+                    };
+                    MySingleton.getInstance(getApplicationContext()).addToRequestQueue(newReq);
+                    //POST request for new user
+                    Intent input = new Intent(getApplicationContext(), LoginActivity.class);
+                    input.putExtra("label_contents", "Make new user");
+                    startActivityForResult(input, 789);
+                }
             }
         });
     }
--- android/Tutorialforlyle/app/src/main/java/lyle/cse216/lehigh/edu/tutorialforlyle/LoginActivity.java
+++ android/Tutorialforlyle/app/src/main/java/lyle/cse216/lehigh/edu/tutorialforlyle/LoginActivity.java
@@ -1,10 +1,22 @@
 package lyle.cse216.lehigh.edu.tutorialforlyle;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 
 /**
@@ -13,29 +25,32 @@ import android.widget.TextView;
  */
 
 public class LoginActivity extends AppCompatActivity{
+    String url = "https://sleepy-dusk-34987.herokuapp.com/users";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
 
-        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = ((TextView)findViewById(R.id.username)).getText().toString();
-                String password = ((TextView)findViewById(R.id.password)).getText().toString();
-                if(username.equals("") || password.equals("")){
+                String username = ((TextView) findViewById(R.id.username)).getText().toString();
+                String password = ((TextView) findViewById(R.id.password)).getText().toString();
+                if (username.equals("") || password.equals("")) {
                     findViewById(R.id.badLoginCreds).setVisibility(View.VISIBLE);
                 } else {
                     Intent input = new Intent(getApplicationContext(), MainActivity.class);
                     input.putExtra("label_contents", "Login");
                     startActivityForResult(input, 789);
+
+
                 }
             }
         });
 
 
-        findViewById(R.id.createUserButton).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.createUserButton).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
@@ -45,8 +60,15 @@ public class LoginActivity extends AppCompatActivity{
             }
         });
 
+
         findViewById(R.id.badLoginCreds).setVisibility(View.INVISIBLE);
 
+        Intent i = getIntent();
+        Log.d("lyle", "label_contents");
+        if (i.getStringExtra("label_contents") != null) {
+            if (i.getStringExtra("label_contents").equals("Make new user"))
+                findViewById(R.id.successCreate).setVisibility(View.VISIBLE);
+        }
     }
 
 }
